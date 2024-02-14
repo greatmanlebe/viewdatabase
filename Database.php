d74f171728a24b0b01c74ea90a4b1277f17b5ae2
greatmanlebe <lebegreatman00@gmail.com>
2024-02-14 11:49:33 +0100

    first commit

--- Database.php
+++ Database.php
@@ -1,10 +1,6 @@
 <?php
-
-function opendatabase($database, $password) {
-    $servername = "localhost"; 
-    $username = "root";     
-         
- $conn = new mysqli($servername, $username, $password, $database);
+function opendatabase($database, $password, $username, $servername ) {
+   $conn = new mysqli($servername, $username, $password, $database);
 
  if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
@@ -18,23 +14,18 @@ function opendatabase($database, $password) {
     if ($result === false) {
         die("Error: " . $conn->error);
     }
-
-    while ($row = $result->fetch_array()) {
+ while ($row = $result->fetch_array()) {
         $tables[] = $row[0];
     }
-
-    
     $conn->close();
-
-    
-    return $tables;
+ return $tables;
 }
-
-
 
 $database = "your data base name";
 $password = "put your data base password";
-$tables = opendatabase($database, $password);
+$username = "enter your user name";
+$servername ="enter your server name";
+$tables = opendatabase($database, $password, $username,   $servername );
 
 foreach ($tables as $table) {
     echo $table . "<br>";
